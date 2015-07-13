1cc3aee07b2e0080392b3c4bf2941614b81dc8ef
Purvesh Patel <p.purvesh.ptl@gmal.com>
2015-07-14 00:47:00 +0530

    Added security and comment ing feature

--- blog/models.py
+++ blog/models.py
@@ -11,9 +11,27 @@ class Post(models.Model):
 
 
 	def publish(self):
-		self.published_date = timezone.now
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
 		return self.title
+	
+	def approved_comments(self):
+		return self.comments.filter(approved=True)
+
+
+class Comment(models.Model):
+	post = models.ForeignKey('blog.Post', related_name='comments')
+	author = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	approved = models.BooleanField(default=False)
+
+	def approve(self):
+		self.approved = True
+		self.save()
+
+	def __str__(self):
+		return self.text
 		
